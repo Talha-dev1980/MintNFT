53543b681b189e780e038011aa58ee3de54dbb7e
Talha-dev1980 <dotdev1980@gmail.com>
2023-02-08 21:02:39 +0500

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import { mintNFT } from ".mint.js";
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("0x145645...12312");
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+  const [balance, setBalance] = useState(null);
+  
+  const [mintNFT, mintedNFT] = useState('Mint NFT');
+
+  async function check() {
+    console.log('Trying to Connect...');
+    if (window.ethereum) {
+      console.log('Metamask found');
+      try {
+        await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        }).then(result => {
+          accountChangedHandler(result[0]);
+          setConnButtonText('Wallet Connected')
+          getAccountBalance(result[0]);
+          console.log(result[0])
+        })
+      } catch (error) {
+        console.log('Error while connecting ' + error);
+      }
+    } else {
+      console.log('metamask doest not Exist');
+    }
+  }
+  async function mint(){
+    await mintNFT();
+    mintedNFT('NFT Minted!')
+  }
+  const accountChangedHandler = (newAccount) => {
+    setWalletAddress(newAccount);
+    getAccountBalance(newAccount.toString())
+  }
+  const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setBalance(ethers.formatEther(balance));
+		})
+		.catch(error => {
+			console.log(error.message);
+		});
+	};
+  window.ethereum.on('accountsChanged', accountChangedHandler);
+
+  /* async function connectWallet() {
+      if(typeof window.ethereum !== 'undefined') {
+        await check();
+  
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        signer.getAddress().then((address) => {
+          console.log(address);
+          return provider.getBalance(address);
+        }).then((balance) => {
+          console.log(balance);
+        });
+      }
+    }*/
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+          onClick={check}>{connButtonText}</button>
+        <h5>Connected Wallet Adress: {walletAddress}</h5>
+        <p>Your Current Balance is: {balance}</p>
+        <button
+          onClick={mint}>{mintNFT}</button>
       </header>
     </div>
   );
---
+++ src/mint.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require('..contract/contract-abi.json')
+const contractAddress = "0xE07D851bD501d452976593885751eDAfbc1883F1";
+export const mintNFT = async() => {
+
+window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': window.contract.methods.safeMint(window.ethereum.selectedAddress).encodeABI()
+};
+
+
+try {
+    const txHash = await window.ethereum
+        .request({
+            method: 'eth_sendTransaction',
+            params: [transactionParameters]
+        });
+    return {
+        success: true,
+        status: "Success minting" + txHash
+    }
+} catch (error) {
+    return {
+        success: false,
+        status: "Something went wrong: " + error.message
+    }
+}
+}
